33770194a77287fe0f3edca1dec5efed6d90aa25
bryonyl <bryonyrlarson@gmail.com>
2024-11-02 15:40:19 +0000

    Continued challenge 5

--- task-5/main.cpp
+++ task-5/main.cpp
@@ -6,24 +6,42 @@ using namespace std;
 
 string convertToLowercaseText(string text)
 {
+    cout << "Lowercase: ";
     for (int i = 0; i <= text.length(); i++)
     {
-        char currentChar = text[i];
-        cout << text[i];
-        //string lowercaseText = tolower(8, currentChar);
+        unsigned char lowercase (tolower(text[i]));
+        cout << lowercase;
     }
-
+    cout << endl;
     return text;
 }
 
 string convertToUppercaseText(string text)
 {
+    cout << "Uppercase: ";
+    for (int i = 0; i <= text.length(); i++)
+    {
+        unsigned char uppercase(toupper(text[i]));
+        cout << uppercase;
+    }
+    cout << endl;
     return text;
 }
 
 string convertToSentenceCase(string text) // Characters should be uppercase only if: 1. it is the first letter of the sentence,
                                           // 2. The character is alphanumeroic and perceded by either a periodmexlamanatiom aro on question mark
 {
+    cout << "Sentence case: ";
+    unsigned char firstChar (toupper(text[0])); // Prints the first character of the sentence as uppercase
+    cout << firstChar;
+
+    for (int i = 0; i <= text.length(); i++)
+    {
+        if (text[i] == '!');
+        {
+            cout << " ";
+        }
+    }
     return text;
 }
 
@@ -34,13 +52,15 @@ string convertToAlternatingCase(string text) // Alternating case should be prese
 
 int main(int argc, char* argv[])
 {
-    cout << "CHALLENGE 5";
-    cout << "-----------";
+    cout << "CHALLENGE 5" << endl;
+    cout << "-----------" << endl;
 
     string text = "";
     cout << "Please input your text: ";
     getline(cin, text);
     convertToLowercaseText(text);
+    convertToUppercaseText(text);
+    convertToSentenceCase(text);
     
     return 0;
 }
